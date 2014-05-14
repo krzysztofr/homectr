88df18d2c3d3147d6716005dc6e31293f899818e
Krzysztof Rakowski <krzysztof@rakowski.pl>
2014-05-14 23:17:59 +0200

    basic argument parsing for user control script

---
+++ user-ctr.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+
+# user control script
+
+import argparse
+
+def list_sessions(args):
+    """Lists sessions in the database file."""
+    print 'list'
+
+def add_session(args):
+    """Adds session to the database file."""
+    print 'add'
+
+def delete_session(args):
+    """Deletes session from the database file."""
+    print 'delete'
+
+def init_db(args):
+    """Initialises database file."""
+    print 'init'
+
+argparser = argparse.ArgumentParser(description="homectr - user database manipulation")
+
+subparsers = argparser.add_subparsers(help='available actions')
+
+list_parser = subparsers.add_parser('list', help='List users')
+list_parser.add_argument('--file', '-f', default='./users.db', action='store', help='database file')
+list_parser.set_defaults(func=list_sessions)
+
+add_parser = subparsers.add_parser('add', help='Add session id')
+add_parser.add_argument('session_id', action='store', help='session id (recommended: 32 characters)')
+add_parser.add_argument('--file', '-f', default='./users.db', action='store', help='database file')
+add_parser.set_defaults(func=add_session)
+
+delete_parser = subparsers.add_parser('delete', help='Delete session id')
+delete_parser.add_argument('session_id', action='store', help='session id to delete')
+delete_parser.add_argument('--file', '-f', default='./users.db', action='store', help='database file')
+delete_parser.set_defaults(func=delete_session)
+
+init_parser = subparsers.add_parser('init', help='Init users database')
+init_parser.add_argument('--file', '-f', default='./users.db', action='store', help='database file')
+init_parser.set_defaults(func=init_db)
+
+args = argparser.parse_args()
+args = args.func(args)
+
+
+
+
