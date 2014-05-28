5d2f7f7cdfc29891c851f2f2cdb18d4c8cb62be7
Krzysztof Rakowski <krzysztof@rakowski.pl>
2014-05-28 23:19:45 +0200

    session checking pulled to dectorator, refs #13

--- homectr-web.py
+++ homectr-web.py
@@ -2,6 +2,8 @@
 
 import datetime
 
+from functools import wraps
+
 from bottle import Bottle, run, static_file, request, template, response, redirect
 
 from settings import devices_definitions, server_params, session_db_file, cookie_secret
@@ -16,30 +18,30 @@ for d in devices_definitions:
     devices[d['pin']] = Device(name=d['name'], pin=d['pin'], action=d['action'])
 
 
-@app.route('/')
-def index():
-    session_id = request.get_cookie('session_id', secret=cookie_secret)
-    if session_id is None:
-        response.status = 403
-        return 'Missing session_id.'
-    with DbSession(session_db_file) as c:
-        result = c.execute('SELECT session_id FROM sessions WHERE session_id = ?;', (session_id,)).fetchone()
-        if result is None:
+def session_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        session_id = request.get_cookie('session_id', secret=cookie_secret)
+        if session_id is None:
             response.status = 403
-            return 'Wrong session_id.'
+            return 'Missing session_id.'
+        with DbSession(session_db_file) as c:
+            result = c.execute('SELECT session_id FROM sessions WHERE session_id = ?;', (session_id,)).fetchone()
+            if result is None:
+                response.status = 403
+                return 'Wrong session_id.'
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@app.route('/')
+@session_required
+def index():
     return template('public/index.html', devices=devices)
 
 @app.route('/switch')
+@session_required
 def switch_device():
-    session_id = request.get_cookie('session_id', secret=cookie_secret)
-    if session_id is None:
-        response.status = 403
-        return 'Missing session_id.'
-    with DbSession(session_db_file) as c:
-        result = c.execute('SELECT session_id FROM sessions WHERE session_id = ?;', (session_id,)).fetchone()
-        if result is None:
-            response.status = 403
-            return 'Wrong session_id.'
     pin = int(request.query.pin)
     devices[pin].switch()
     return "OK"
